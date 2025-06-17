b4db8a0037248477af70179926c6e80ad6b2e37b
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-06-17 15:47:45 -0400

    update main.py for conversations and verbose opts

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-client = genai.Client(api_key=api_key)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # My original code
+    # if len(sys.argv) == 1:
+    #     print("Error: No prompt entered.")
+    #     sys.exit(1)
+
+    #Boot.Dev's code, which I like better
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+    )
+
+    print(response.text)
+
+    if "--verbose" in args:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+
+if __name__ == "__main__":
+    main()
