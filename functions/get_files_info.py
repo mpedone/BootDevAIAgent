6feb77b447f20bc248f4ba0c588b680b85cf7e3a
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-06-23 13:03:50 -0400

    finish get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,23 +1,33 @@
 import os
-from os.path import getsize, isdir, abspath
+from os.path import getsize, isdir, abspath, join
 
 def get_files_info(working_directory, directory=None):
-    working_directory = abspath(working_directory)
-    target_directory = os.path.join(working_directory, directory)
+    if not working_directory:
+        return f'Error: missing 1 required positional argument: "working_directory"'
+    if not directory:
+        directory = '.'
 
-    if directory not in os.listdir(working_directory) and directory != '.':
+    full_working_directory = abspath(working_directory)
+    target_directory = join(full_working_directory, directory)
+
+    if not os.path.exists(full_working_directory):
+        return f'Error: No such file or directory: "{working_directory}"'
+    if not isdir(full_working_directory):
+        return f'Error: "{working_directory}" is not a directory'
+
+    if directory not in os.listdir(full_working_directory) and directory != '.':
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not isdir(target_directory):
         return f'Error: "{directory}" is not a directory'
-    
-    os.chdir(target_directory)
 
-    files = os.listdir()
+    files = os.listdir(target_directory)
 
-    file_info_list = [f'- {file}: file_size={getsize(abspath(file))} bytes, is_dir={isdir(abspath(file))}' for file in files]
+    file_info_list = [f'- {file}: file_size={getsize(join(target_directory,file))} bytes, is_dir={isdir(join(target_directory, file))}' for file in files]
 
     file_info = "\n".join(file_info_list)
 
     return file_info
 
-print(get_files_info("calculator", "."))
+
+# print(get_files_info("calculator", "."))
+print(get_files_info(".", "pkg"))
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 # tests in the main folder
 
-import unittest
+import unittest, os
 from functions.get_files_info import get_files_info
 
 class TestGetFilesInfo(unittest.TestCase):
@@ -8,15 +8,84 @@ class TestGetFilesInfo(unittest.TestCase):
         test_output = get_files_info("calculator", "pkg")
         expected = "- __pycache__: file_size=4096 bytes, is_dir=True\n- render.py: file_size=766 bytes, is_dir=False\n- calculator.py: file_size=1737 bytes, is_dir=False"
         print(test_output)
-        print(expected)
         self.assertEqual(test_output, expected)
     
     def test_same_dir(self):
         test_output = get_files_info("calculator", ".")
         expected = "- pkg: file_size=4096 bytes, is_dir=True\n- main.py: file_size=575 bytes, is_dir=False\n- tests.py: file_size=1342 bytes, is_dir=False"
         print(test_output)
-        print(expected)
         self.assertEqual(test_output, expected)
+
+    def test_not_in_dir(self):
+        test_output = get_files_info("calculator", "/bin")
+        expected = 'Error: Cannot list "/bin" as it is outside the permitted working directory'
+        print(test_output)
+        self.assertEqual(test_output, expected)
+
+    def test_parent_not_in_dir(self):
+        test_output = get_files_info("calculator", "../")
+        expected = 'Error: Cannot list "../" as it is outside the permitted working directory'
+        print(test_output)
+        self.assertEqual(test_output, expected)
+    
+    def test_not_a_dir(self):
+        test_output = get_files_info("calculator", "main.py")
+        expected = 'Error: "main.py" is not a directory'
+        self.assertEqual(test_output, expected)
+
+    def test_subdir(self):
+        test_output = get_files_info("calculator/pkg", "__pycache__")
+        self.assertIn("calculator.cpython", test_output)
+    
+    def test_absolute_path(self):
+        test_output = get_files_info("/home/mattpedone/workspace/GITHUB", "mpedone")
+        self.assertIn("BootDevAIAgent", test_output)
+
+    def test_absolute_path_self_ref(self):
+        test_output = get_files_info("/home/mattpedone/workspace/GITHUB/mpedone", ".")
+        self.assertIn("BootDevAIAgent", test_output)
+
+    def test_no_target(self):
+        test_output = get_files_info("calculator")
+        expected = "- pkg: file_size=4096 bytes, is_dir=True\n- main.py: file_size=575 bytes, is_dir=False\n- tests.py: file_size=1342 bytes, is_dir=False"
+        self.assertEqual(test_output, expected)
+    
+    def test_blank_target(self):
+        test_output = get_files_info("calculator", "")
+        expected = "- pkg: file_size=4096 bytes, is_dir=True\n- main.py: file_size=575 bytes, is_dir=False\n- tests.py: file_size=1342 bytes, is_dir=False"
+        self.assertEqual(test_output, expected)
+    
+    def test_space_target(self):
+        test_output = get_files_info("calculator", " ")
+        expected = 'Error: Cannot list " " as it is outside the permitted working directory'
+        self.assertEqual(test_output, expected)
+    
+    def test_blank_working_directory(self):
+        test_output = get_files_info("anything", "calculator")
+        expected = f'Error: No such file or directory: "anything"'
+        self.assertEqual(test_output, expected)
+
+    def test_working_directory_not_dir(self):
+        test_output = get_files_info("main.py", "calculator")
+        expected = f'Error: "main.py" is not a directory'
+        self.assertEqual(test_output, expected)
+    
+    # def test_abs_path(self):
+    #     test_output = get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent", "/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent/calculator")
+    #     expected = "- pkg: file_size=4096 bytes, is_dir=True\n- main.py: file_size=575 bytes, is_dir=False\n- tests.py: file_size=1342 bytes, is_dir=False"
+    #     self.assertEqual(test_output, expected)
+
+# get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent", "/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent/calculator")
+
+# get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent", "/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent/.")
+
+# get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent/calculator", "/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent")
+
+# get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent", "/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent/..")
+
+# get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent", "/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent/main.py")
+
+# get_files_info("/home/mattpedone/workspace/GITHUB/mpedone/BootDevAIAgent", "/home/mattpedone/workspace/GITHUB/mpedone/StaticSite/Public")
 
 
 if __name__ == "__main__":
